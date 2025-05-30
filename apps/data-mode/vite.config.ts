405f986519171c20d38fe785a6d8df7c5a1b1a37
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2025-05-30 13:39:36 +0200

    add data mode

--- apps/data-mode/vite.config.ts
+++ apps/data-mode/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig(() => ({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/declarative-mode',
+  cacheDir: '../../node_modules/.vite/apps/data-mode',
   server: {
     port: 4200,
     host: 'localhost',
